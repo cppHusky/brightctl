41d4dbdf3383bebb83e9a78e483365a685ac06bd
cppHusky <cpphusky@outlook.com>
2025-03-25 10:48:43 +0800

    supported different backlight gpus modified:   Cargo.toml modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ enum Op{
 	Inc(i32),
 	Dec(i32),
 }
-fn main()->Result<(),std::io::Error>{
+fn main()->Result<(),Box<dyn std::error::Error>>{
 	let op=std::env::args().nth(1).expect("The first argument is empty!");
 	let delta:i32=std::env::args().nth(2).expect("The second argument is empty!").parse().expect("The second argument must be unsigned integer!");
 	let op=match op.as_str(){
@@ -14,10 +14,12 @@ fn main()->Result<(),std::io::Error>{
 		"dec"=>{Op::Dec(delta)},
 		_=>{panic!("The first argument should be either `inc` or `dec`.")},
 	};
-	let max_brightness=read_val("/sys/class/backlight/amdgpu_bl1/max_brightness")?;
-	let brightness=read_val("/sys/class/backlight/amdgpu_bl1/brightness")?;
-	eprintln!("Previous brightness is {brightness}/{max_brightness}");
-	let mut level=((STEPS as f64)*((brightness as f64)/(max_brightness as f64)).powf(1./GAMMA)).round() as i32;
+	let max_brightness_path=get_path("max_brightness")?;
+	let brightness_path=get_path("brightness")?;
+	let max_brightness=read_val(&max_brightness_path)?;
+	let previous_brightness=read_val(&brightness_path)?;
+	eprintln!("Previous brightness is {previous_brightness}/{max_brightness}");
+	let mut level=((STEPS as f64)*((previous_brightness as f64)/(max_brightness as f64)).powf(1./GAMMA)).round() as i32;
 	eprintln!("Previous brightness level is {level}.");
 	match op{
 		Op::Inc(i)=>{
@@ -37,23 +39,28 @@ fn main()->Result<(),std::io::Error>{
 	};
 	eprintln!("Current brightness level is {level}.");
 	let mut new_brightness=((max_brightness as f64)*((level as f64)/(STEPS as f64)).powf(GAMMA)).round() as i32;
-	if new_brightness==brightness{
+	if new_brightness==previous_brightness{
 		match op{
-			Op::Inc(_)=>{new_brightness=std::cmp::min(brightness+1,max_brightness);}
-			Op::Dec(_)=>{new_brightness=std::cmp::max(brightness-1,0);}
+			Op::Inc(_)=>{new_brightness=std::cmp::min(previous_brightness+1,max_brightness);}
+			Op::Dec(_)=>{new_brightness=std::cmp::max(previous_brightness-1,0);}
 		}
 	}
 	eprintln!("Current brightness is {new_brightness}/{max_brightness}");
-	write_val("/sys/class/backlight/amdgpu_bl1/brightness",new_brightness)?;
+	write_val(&brightness_path,new_brightness)?;
 	Ok(())
 }
-fn read_val(path:&str)->Result<i32,std::io::Error>{
-	let f=std::fs::File::open(path).expect(format!("Cannot open {path}").as_str());
+fn get_path(tail:&str)->Result<std::path::PathBuf,Box<dyn std::error::Error>>{
+	let mut entry=std::fs::read_dir("/sys/class/backlight")?.nth(0).unwrap().unwrap().path();
+	entry.push(tail);
+	Ok(entry)
+}
+fn read_val(path:&std::path::PathBuf)->Result<i32,std::io::Error>{
+	let f=std::fs::File::open(path).expect(format!("Cannot open {}",path.display()).as_str());
 	let reader=std::io::BufReader::new(f);
 	Ok(reader.lines().next().unwrap()?.parse::<i32>().unwrap())
 }
-fn write_val(path:&str,val:i32)->Result<(),std::io::Error>{
-	let f=std::fs::OpenOptions::new().write(true).open(path).expect(format!("Cannot open {path}").as_str());
+fn write_val(path:&std::path::PathBuf,val:i32)->Result<(),std::io::Error>{
+	let f=std::fs::OpenOptions::new().write(true).open(path).expect(format!("Cannot open {}",path.display()).as_str());
 	let mut writer=std::io::BufWriter::new(f);
 	writer.write(val.to_string().as_bytes())?;
 	Ok(())
